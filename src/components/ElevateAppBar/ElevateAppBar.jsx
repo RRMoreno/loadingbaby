d5ba54454cf564527d5c2885685af4df66d16cf7
Roxana M. Rafols Moreno <roxanarafols94@gmail.com>
2021-08-12 20:22:38 +0200

    Agregando el nav

--- src/components/ElevateAppBar/ElevateAppBar.jsx
+++ src/components/ElevateAppBar/ElevateAppBar.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import {makeStyles} from "@material-ui/core";
 
 function ElevationScroll(props) {
-    const { children, window } = props;
+    const {children, window} = props;
 
     const trigger = useScrollTrigger({
         disableHysteresis: true,
@@ -25,20 +27,36 @@ function ElevationScroll(props) {
 ElevationScroll.propTypes = {
     children: PropTypes.element.isRequired,
     window: PropTypes.func,
-};
-
+}
+const useStyles = makeStyles(() => ({
+    nav: {
+        display: "flex",
+        justifyContent: 'space-between',
+        width: "100%"
+    },
+}));
 export default function ElevateAppBar(props) {
+    const classes = useStyles();
     return (
         <React.Fragment>
-            <CssBaseline />
+            <CssBaseline/>
             <ElevationScroll {...props}>
                 <AppBar>
                     <Toolbar>
-                        <Typography variant="h6">Scroll to Elevate App Bar</Typography>
+                        <div className={classes.nav}>
+                            <div>
+                                <SkipPreviousIcon/>
+                                <NavigateBeforeIcon/>
+                            </div>
+                            <div>
+                                <NavigateNextIcon/>
+                                <SkipNextIcon/>
+                            </div>
+                        </div>
+
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
-            <Toolbar />
         </React.Fragment>
     );
 }
