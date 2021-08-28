87d8ae9a82b31d60b7c5cfae76966ded2422cf8d
Roxana M. Rafols Moreno <roxanarafols94@gmail.com>
2021-08-28 22:14:12 +0200

    *Nuevo componente agregado: PrimerTrimestre.jsx *Comenzando el soporte multi idioma con i18n

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -20,11 +20,11 @@ function Hello() {
         <div className={classes.images + ' container-sm'}>
             <img src={hello} width={400} alt="Hello" />
             <h2>Hoy queremos compartir una gran noticia</h2>
+            <p>Recomendamos deslizar hacia abajo en todas las pantallas.</p>
             <Typography className={classes.root}>
                 <Link href="/news">
                     Click para continuar
                 </Link>
-
             </Typography>
         </div>
 
